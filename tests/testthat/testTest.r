836d95cb79044384d733b41a72b444e8ce9918b0
Tom Jelen <tj@blackwoodseven.com>
2015-03-06 13:00:33 +0100

    a test that accesses the package code

--- tests/testthat/testTest.r
+++ tests/testthat/testTest.r
@@ -3,3 +3,8 @@ context("my first test")
 test_that("1 is 1", {
   expect_equal(1, 1)
 })
+
+test_that("can access package code", {
+  theMatrix <- getMatrix()
+  expect_equal(theMatrix["mandag", "eur"], 7.5)  
+})

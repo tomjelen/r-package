f38cd09d592fe56232d38dd5529b33c7281acc22
Tom Jelen <tj@blackwoodseven.com>
2015-03-06 15:52:54 +0100

    get and use data from one of our micro-services

--- R/Jelen.R
+++ R/Jelen.R
@@ -12,3 +12,18 @@ fetchJson <- function() {
   response <-jsonlite::fromJSON("http://citibikenyc.com/stations/json")
   return(response)
 }
+
+# go-rest-service experiment
+getTimeSeries <- function() {
+  usdJson <- jsonlite::fromJSON("http://stage.blackwoodseven.com:4444/?currency=USD&from=2015-03-01&to=2015-03-05")
+  usdTimeserie = usdJson$series$samples[[1]]$value
+
+  eurJson <- jsonlite::fromJSON("http://stage.blackwoodseven.com:4444/?currency=EUR&from=2015-03-01&to=2015-03-05")
+  eurTimeserie = eurJson$series$samples[[1]]$value
+  
+  fakeClicksTimeserie = c(1000, 200, 300, 500)
+  
+  result <- data.frame(USD=usdTimeserie, EUR=eurTimeserie, AdformClicks=fakeClicksTimeserie)
+  row.names(result) <- c("2015-03-02", "2015-03-03", "2015-03-04", "2015-03-05")
+  return(result)
+}
